e1fd9a9454dbf24735b2d9d09b619b5b9c7ecb65
Akbarkz <akbarykhan@gmail.com>
2019-06-18 16:12:50 +0600

    band aid fix #6397

--- src/main/webapp/scripts/nodes/controller/nodeCtrl.js
+++ src/main/webapp/scripts/nodes/controller/nodeCtrl.js
@@ -42,9 +42,19 @@
 		self.getNodeTreeData = function(){
 			TreeService.getNodeTree(4).then(function(response){
 				if(response.status == 200){
+					response.data[0].childNodes[0].India = response.data[0].childNodes[0].inputValue;
+					response.data[0].childNodes[0].COTI = response.data[0].childNodes[1].inputValue;
+					response.data[0].childNodes[0].childNodes[0].India = response.data[0].childNodes[0].childNodes[0].inputValue;
+					response.data[0].childNodes[0].childNodes[1].India = response.data[0].childNodes[0].childNodes[1].inputValue;
+					
+					
+					response.data[0].childNodes[0].childNodes[0].COTI = response.data[0].childNodes[1].childNodes[0].inputValue;
+					response.data[0].childNodes[0].childNodes[1].COTI = response.data[0].childNodes[1].childNodes[1].inputValue;
+					response.data[0].childNodes[1] = null;
+					self.selectedCountry = 1;
 					writeoutNode( response.data, 0, $scope.gridOptions.data );
-					self.selectedCountry = 2;
-					self.filterByCountry(2);
+					
+					self.filterByCountry(1);
 					
 				}
 			});
@@ -73,38 +83,44 @@
 		self.getNodeTreeData();
 		self.filterByCountry = function(countryId){
 			self.generateColumn(countryId);
-			if(countryId == 1){
-				$scope.gridOptions.data = $scope.data;
-			}else{
-				var country = self.country.filter(function(c){ return c.id == countryId});
-				var filteredData = $scope.data.filter(function(d){
-					if (d.region == country[0].name || d.region == 'undefined' || d.region == null){
-						return d;
-					}
-					});
-				$scope.gridOptions.data = filteredData;
-			}
+//			if(countryId == 1){
+//				$scope.gridOptions.data = $scope.data;
+//			}else{
+//				var country = self.country.filter(function(c){ return c.id == countryId});
+//				var filteredData = $scope.data.filter(function(d){
+//					if (d.region == country[0].name || d.region == 'undefined' || d.region == null){
+//						return d;
+//					}
+//					});
+//				$scope.gridOptions.data = filteredData;
+//			}
 		}
 		var writeoutNode = function( childArray, currentLevel, dataArray ){
 			  childArray.forEach( function( node ){
-			    if ( node.childNodes !== null && node.childNodes !== 'undefined' && node.childNodes.length > 0 ){
-			      node.$$treeLevel = currentLevel;
-			    }
-			    dataArray.push( node );
-			    if(node.childNodes !== null && node.childNodes !== 'undefined'){
-			    	writeoutNode( node.childNodes, currentLevel + 1, dataArray );
-			    }
+				  if(node !== null){
+					  if ( node.childNodes !== null && node.childNodes !== 'undefined' && node.childNodes.length > 0 ){
+					      node.$$treeLevel = currentLevel;
+					    }
+					    dataArray.push( node );
+					    if(node.childNodes !== null && node.childNodes !== 'undefined'){
+					    	writeoutNode( node.childNodes, currentLevel + 1, dataArray );
+					    }
+				  }
+			    
 			  });
 			  /*Add region as key and input value as region key value*/
-			  $scope.gridOptions.data.forEach(function(d){
-					if(d.region){
-						var c = d.region;
-						d[c] = d.inputValue;
-					}
-				});
+//			  $scope.gridOptions.data.forEach(function(d){
+//					if(d.region){
+//						var c = d.region;
+//						d[c] = d.inputValue;
+//					}
+//				});
 			  $scope.data = angular.copy(dataArray);
 			};
-
+			
+		self.temporaryRegionMapping = function(dataArray){
+			
+		}	
 			
 		  $scope.gridOptions = {
 				  	data: [],
